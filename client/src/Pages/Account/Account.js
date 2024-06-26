8a065a58a23874fd64f969020632585d5b76437b
Daniel L <1256432378l@gmail.com>
2024-06-26 14:50:02 +1000

    upload profile image to AWS s3

--- client/src/Pages/Account/Account.js
+++ client/src/Pages/Account/Account.js
@@ -1,27 +1,69 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 //import image from '../../Assets/images/chooseImage.png'
 import portrait from '../../Assets/images/portrait.jpg'
 import './Account.css'
 
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import imageCompression from 'browser-image-compression';
+
+window.Buffer = window.Buffer || require("buffer").Buffer;
+
 export const Account = () => {
     const [imageFile, setImageFile] = useState(null);
-    const [imageFileURL, setImageFileUrl] = useState(null);
+    const [imageFileURL, setImageFileURL] = useState(null);
     const fileRef = useRef();
 
-    function binaryToBlob(binaryData, contentType) {
+    const binaryToBlob = (binaryData, contentType) => {
         return new Blob([binaryData], {type: contentType});
     }
     
     const handleImageChange = (e) => {
         const file = e.target.files[0];
         if (file) {
+            setImageFileURL(URL.createObjectURL(binaryToBlob(imageFile)));
             setImageFile(file);
-            // console.log(imageFile instanceof File); 
-            // console.log(imageFile instanceof Blob);
-            setImageFileUrl(URL.createObjectURL(binaryToBlob(imageFile)));
         }
-        console.log(imageFile, imageFileURL);
     }
+    console.log("File:", imageFile);
+    console.log("File URL:", imageFileURL);
+
+    useEffect(() => {
+        if (imageFile) {
+            uploadImage();
+        }
+    }, [imageFile]);
+
+    const uploadImage = async () => {
+        const options = {
+            maxSizeMB: 1,
+            maxWidthOrHeight: 300,
+            useWebWorker: true
+        }
+        const s3 = new S3Client({
+            region: process.env.REACT_APP_AWS_REGION,
+            credentials: {
+                accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY
+            }
+        });
+        try {
+            const compressedFile = await imageCompression(imageFile, options);
+            console.log('compressedFile instanceof Blob', compressedFile instanceof Blob); // true
+            console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
+
+            const uploadParams = {
+                Bucket: process.env.REACT_APP_S3_BUCKET_NAME,
+                Key: compressedFile.name,
+                Body: compressedFile,
+            };
+            const command = new PutObjectCommand(uploadParams);
+            const data = await s3.send(command);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
   return (
     <>
         <section className='accountinfo'>
@@ -29,23 +71,21 @@ export const Account = () => {
                 <h1>Account Infomation</h1>
                 <div className='content'>
                     <div className='left'>
-                        <form>
-                            <input type='file' onChange={handleImageChange} accept='image/*' ref={fileRef} hidden/>
-                        </form>
+                        <input type='file' onChange={handleImageChange} accept='image/*' ref={fileRef} hidden/>
                         <img src={imageFileURL || portrait} alt='images' onClick={() => fileRef.current.click()} />
                     </div>
                         <div className='right'>
                             {/* will show current username */}
-                            <label>Username</label>
-                            <input type='text' />
+                            <label htmlFor='username'>Username</label>
+                            <input type='text' id='username'/>
 
                             {/* will show current email */}
-                            <label>Email</label>
-                            <input type='email' />
+                            <label htmlFor='email'>Email</label>
+                            <input type='email' id='email' />
 
                             {/* won't show current password, but can input new password */}
-                            <label>Password</label>
-                            <input type='password' />
+                            <label htmlFor='password'>Password</label>
+                            <input type='password' id='password' />
 
                             {/* submit button */}
                             <button className='button'>Update</button>
