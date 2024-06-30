89baa4117d6b0b6d9138b91c2c0bb69019b67ec3
Daniel L <1256432378l@gmail.com>
2024-06-30 16:09:27 +1000

    get posts api

--- Backend/controllers/post.controller.js
+++ Backend/controllers/post.controller.js
@@ -23,3 +23,50 @@ export const createPost = async (req, res, next) => {
         next(error);
     }
 }
+
+export const getPosts = async (req, res, next) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex || 0);
+        const limit = parseInt(req.query.limit || 6);
+        const sortDirection = req.query.order === "asc" ? 1: -1;
+
+        const posts = await Post.find({
+            ...(req.query.userId && {userId: req.query.userId}),
+            ...(req.query.category && {category: req.query.category}),
+            ...(req.query.slug && {slug: req.query.slug}),
+            ...(req.query.postId && {_id: req.query.postId}),
+            ...(req.query.searchTerm && {
+                $or: [
+                    {title: {$regex: req.query.searchTerm, $options: 'i'}},
+                    {content: {$regex: req.query.searchTerm, $options: 'i'}}
+                ]
+            }),
+            })
+            .sort({updatedAt: sortDirection})
+            .skip(startIndex)
+            .limit(limit);
+
+    //     const startIndex = parseInt(req.query.startIndex) || 0;
+    //     const limit = parseInt(req.query.limit) || 6;
+    //     const sortDirection = req.query.order === 'asc' ? 1 : -1;
+    //     const posts = await Post.find({
+    //         ...(req.query.userId && { userId: req.query.userId }),
+    //         ...(req.query.category && { category: req.query.category }),
+    //         ...(req.query.slug && { slug: req.query.slug }),
+    //         ...(req.query.postId && { _id: req.query.postId }),
+    //         ...(req.query.searchTerm && {
+    //             $or: [
+    //             { title: { $regex: req.query.searchTerm, $options: 'i' } },
+    //             { content: { $regex: req.query.searchTerm, $options: 'i' } },
+    //             ],
+    //     }),
+    //     })
+    //   .sort({ updatedAt: sortDirection })
+    //   .skip(startIndex)
+    //   .limit(limit);
+
+        res.status(200).json({posts});
+    } catch (error) {
+        next (error);
+    }
+}
