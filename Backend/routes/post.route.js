b8b68b82279f61dbc5ca6bc98a56a911eec6d63f
Daniel L <1256432378l@gmail.com>
2024-07-01 10:02:25 +1000

    delete post api

--- Backend/routes/post.route.js
+++ Backend/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createPost, getPosts } from '../controllers/post.controller.js';
+import { createPost, deletePost, getPosts } from '../controllers/post.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createPost);
 router.get('/getPosts', getPosts);
+router.delete('/deletePost/:postId/:userId', verifyToken, deletePost);
 
 export default router;
