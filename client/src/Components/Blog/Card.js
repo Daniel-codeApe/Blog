2e21cefdf749307c5dbd7b079f51897abde0ef4a
Daniel L <1256432378l@gmail.com>
2024-07-04 16:35:34 +1000

    update comment count on home page

--- client/src/Components/Blog/Card.js
+++ client/src/Components/Blog/Card.js
@@ -5,62 +5,82 @@ import { AiOutlineClockCircle, AiOutlineComment, AiOutlineShareAlt, AiOutlineTag
 import { Link } from 'react-router-dom';
 // import {useSelector} from 'react-redux';
 
-export const Card = () => {
-    // const {currentUser} = useSelector((state) => state.user);
+const Card = () => {
     const [posts, setPosts] = useState([]);
+    const [commentsCount, setCommentsCount] = useState({});
+
+    const getNumberofComments = async (postId) => {
+        try {
+            const res = await fetch(`/api/comment/get/${postId}`);
+            if (res.ok) {
+                const data = await res.json();
+                return data.length;
+            }
+        } catch (error) {
+            console.log(error.message);
+            return 0;
+        }
+    };
 
     useEffect(() => {
         const fetchPosts = async () => {
             try {
                 const res = await fetch('/api/post/getPosts', {});
-                const { posts } = await res.json(); // Extract posts array
-                setPosts(posts); // Set the posts state to the array
+                const { posts } = await res.json();
+                setPosts(posts);
+
+                const commentsCounts = await Promise.all(
+                    posts.map(async (post) => {
+                        const count = await getNumberofComments(post._id);
+                        return { postId: post._id, count };
+                    })
+                );
+
+                const commentsCountMap = {};
+                commentsCounts.forEach(({ postId, count }) => {
+                    commentsCountMap[postId] = count;
+                });
+
+                setCommentsCount(commentsCountMap);
             } catch (error) {
                 console.log(error.message);
             }
-        }
+        };
         fetchPosts();
     }, []);
 
-    // console.log(posts);
-    // console.log(Array.isArray(posts));
-  return (
-    <>
-        <section className='blog'>
-            <div className='container grid3'>
-                {posts.map((item) => (
-                    <div className='box boxitems' key={item._id}>
-                        <div className='img'>
-                            {/* link to the detail page when click on the title */}
-                            <Link to={`/details/${item._id}`}>
-                                <img src={item.coverImage} alt='cover' />
-                            </Link>
-                        </div>
-                        <div className='details'>
-                            <div className='tags'>
-                                <AiOutlineTags className='icon' />
-                                {/* Make a link for every blog card's category, suppose to lead to 
-                                a page the includes every blog within that category */}
-                                <a href='/'>#{item.category}</a>
+    return (
+        <>
+            <section className='blog'>
+                <div className='container grid3'>
+                    {posts.map((item) => (
+                        <div className='box boxitems' key={item._id}>
+                            <div className='img'>
+                                <Link to={`/details/${item._id}`}>
+                                    <img src={item.coverImage} alt='cover' />
+                                </Link>
                             </div>
-                            {/* link to the detail page when click on the title */}
-                            <Link to={`/details/${item._id}`} className='link'>
-                                <h3>{item.title}</h3>
-                            </Link>
-                            <p>{item.content.slice(0, 180)} ...</p>
-                            {/* Icons for created date, number of comments, and share */}
-                            <div className='date'>
-                                <AiOutlineClockCircle className='icon' /> <label>{new Date(item.updatedAt).toLocaleDateString()}</label>
-                                <AiOutlineComment className='icon' /> <label>27</label>
-                                <AiOutlineShareAlt className='icon' /> <label>SHARE</label>
+                            <div className='details'>
+                                <div className='tags'>
+                                    <AiOutlineTags className='icon' />
+                                    <a href='/'>#{item.category}</a>
+                                </div>
+                                <Link to={`/details/${item._id}`} className='link'>
+                                    <h3>{item.title}</h3>
+                                </Link>
+                                <p>{item.content.slice(0, 180)} ...</p>
+                                <div className='date'>
+                                    <AiOutlineClockCircle className='icon' /> <label>{new Date(item.updatedAt).toLocaleDateString()}</label>
+                                    <AiOutlineComment className='icon' /> <label>{commentsCount[item._id] || 0}</label>
+                                    <AiOutlineShareAlt className='icon' /> <label>SHARE</label>
+                                </div>
                             </div>
                         </div>
+                    ))}
                 </div>
-                ))}
-            </div>
-        </section>
-    </>
-  )
-}
+            </section>
+        </>
+    );
+};
 
-export default Card
+export default Card;
